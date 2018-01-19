bc1f0dc8f91145c08f44137a8cc41607971b40d3
DESKTOP-I290AE4\Rsheng <yunsun60110@gmail.com>
2018-01-19 19:31:22 +0800

    package.json

--- index.ts
+++
@@ -1,9 +0,0 @@
-import { WechatClient } from './src'
-import { WECHAT } from './config'
-
-const wechat = new WechatClient(WECHAT)
-
-wechat.uploadMedia("image", "https://storage.googleapis.com/paas-storage/3-2_dress.jpg")    
-    .then(mediaId => {
-        console.log(mediaId)
-    })
