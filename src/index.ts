d0b61778aba678832ed344264628fb3bd7186fe1
DESKTOP-I290AE4\Rsheng <yunsun60110@gmail.com>
2018-01-24 15:26:47 +0800

    no message

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Client from './client'
 import LineClient from './line'
 import WechatClient from './wechat'
+import MessengerClient from './messenger'
 
-export { Client, LineClient, WechatClient }
+export { Client, LineClient, MessengerClient, WechatClient }
